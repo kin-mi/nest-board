f0be2708101f15c40672f98f56a20bf38e6b965f
Kin mi <backtothenearfuture@gmail.com>
2020-03-20 10:02:46 +0900

    gapi の読み込みをheadに戻す

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -60,7 +60,7 @@
         <br />
         <button
           v-if="isLoggedIn"
-          @click="$googleSignOut"
+          @click="$googleSignOut(gapi)"
           class="mx-auto mt-6 bg-white hover:bg-gray-100 text-gray-800 font-semibold py-1 px-2 border border-gray-400 rounded shadow"
         >
           ログアウト
@@ -98,6 +98,7 @@ export default {
   },
   data() {
     return {
+      gapi: null,
       inputAlbumTitle: 'Nest Board',
       inputPhotoTitle: 'nest-board.png',
       albumList: [],
@@ -107,11 +108,32 @@ export default {
       contentProgress: 0
     }
   },
+  head() {
+    return {
+      // head内でcallbackを使う
+      // https://vueschool.io/articles/vuejs-tutorials/how-to-load-third-party-scripts-in-nuxt-js/
+      script: [
+        {
+          src: 'https://apis.google.com/js/api.js',
+          type: 'text/javascript',
+          callback: () => {
+            this.$gapiInit(window.gapi)
+            this.gapi = window.gapi
+          }
+        }
+      ]
+    }
+  },
   computed: {
     ...mapGetters(['ready']),
     ...mapGetters('auth', ['isLoggedIn'])
   },
   watch: {
+    gapi(newVal, oldVal) {
+      if (newVal) {
+        this.$googleCurrentUser(this.gapi)
+      }
+    },
     ready(newVal, oldVal) {
       if (newVal) {
         // 処理中の場合、送信処理を続行する
@@ -142,7 +164,7 @@ export default {
         // 描画情報をlocal storageへ保存
         this.$refs.draw.save()
         // google sign in
-        this.$googleSignIn()
+        this.$googleSignIn(this.gapi)
       }
     },
     /**
@@ -150,9 +172,9 @@ export default {
      * @return {Object} Album
      */
     async createAlbum() {
-      const ret = await this.$gapiInit().then(() => {
+      const ret = await this.$gapiInit(this.gapi).then(() => {
         // Doc: https://developers.google.com/photos/library/reference/rest/v1/albums/create
-        return this.$gapi.client.photoslibrary.albums
+        return this.gapi.client.photoslibrary.albums
           .create({
             album: {
               title: this.inputAlbumTitle
@@ -172,10 +194,10 @@ export default {
       const config = {
         pageSize: 50
       }
-      await this.$gapiInit().then(async () => {
+      await this.$gapiInit(this.gapi).then(async () => {
         const call = async (config) => {
           // Doc: https://developers.google.com/photos/library/reference/rest/v1/albums/list
-          await this.$gapi.client.photoslibrary.albums
+          await this.gapi.client.photoslibrary.albums
             .list(config)
             .then((response) => {
               // 0件の場合もある
@@ -211,84 +233,88 @@ export default {
         this.saveAndLogin()
       }
       this.contentProgress = 25
-
-      // アルバム一覧の取得
-      await this.fetchAlbumList().catch((e) =>
-        this.onError('Failed fetch Album List.', e)
-      )
-      this.contentProgress = 40
-
-      // 同名アルバムが存在しない場合、作成する
-      // Album Response
-      // Doc: https://developers.google.com/photos/library/reference/rest/v1/albums#resource:-album
-      let album = this.albumList.find((e) => e.title === this.inputAlbumTitle)
-      if (!album) {
-        album = await this.createAlbum().catch((e) =>
-          this.onError('Failed Create Album.', e)
+      try {
+        // アルバム一覧の取得
+        await this.fetchAlbumList().catch((e) =>
+          this.onError('Failed fetch Album List.', e)
         )
-      }
-      this.contentProgress = 50
+        this.contentProgress = 40
 
-      // 描画情報の取得（Base64）
-      this.imageData = await this.$refs.draw.dataURL()
-
-      if (album) {
-        // 認証情報の取得
-        // GoogleAuth
-        // Doc: https://developers.google.com/identity/sign-in/web/reference#googleauththenoninit_onerror
-        const auth = await this.$googleAuth()
-        // GoogleUser
-        // Doc: https://developers.google.com/identity/sign-in/web/reference#googleusergetid
-        const googleUser = auth.currentUser.get()
-        const authResponse = googleUser.getAuthResponse(true)
-        const accessToken = authResponse.access_token
-        if (!accessToken) {
-          this.onError('Failed get Authorization Data.')
+        // 同名アルバムが存在しない場合、作成する
+        // Album Response
+        // Doc: https://developers.google.com/photos/library/reference/rest/v1/albums#resource:-album
+        let album = this.albumList.find((e) => e.title === this.inputAlbumTitle)
+        if (!album) {
+          album = await this.createAlbum().catch((e) =>
+            this.onError('Failed Create Album.', e)
+          )
         }
-        this.contentProgress = 60
+        this.contentProgress = 50
 
-        // 画像をGoogleへアップロード(Netlify Function)
-        // Doc: https://developers.google.com/photos/library/guides/upload-media
-        const res = await this.$axios
-          .post('/postPhoto', {
-            params: {
-              accessToken,
-              photoURL: this.imageData
-            }
-          })
-          .catch((e) => this.onError('Failed POST Photo.', e))
-        const uploadToken = res.data
-        this.contentProgress = 75
+        // 描画情報の取得（Base64）
+        this.imageData = await this.$refs.draw.dataURL()
 
-        if (uploadToken) {
-          this.contentProgress = 95
-          // アップロードした画像をアルバムに追加
-          const result = await this.$gapiInit().then(async () => {
-            // Doc: https://developers.google.com/photos/library/reference/rest/v1/mediaItems/batchCreate
-            const ret = await this.$gapi.client.photoslibrary.mediaItems.batchCreate(
-              {
-                albumId: album.id,
-                newMediaItems: [
-                  {
-                    description: 'Created by Nest Board.',
-                    simpleMediaItem: {
-                      fileName: this.inputPhotoTitle,
-                      uploadToken
-                    }
-                  }
-                ]
+        if (album) {
+          // 認証情報の取得
+          // GoogleAuth
+          // Doc: https://developers.google.com/identity/sign-in/web/reference#googleauththenoninit_onerror
+          const auth = await this.$googleAuth(this.gapi)
+          // GoogleUser
+          // Doc: https://developers.google.com/identity/sign-in/web/reference#googleusergetid
+          const googleUser = auth.currentUser.get()
+          const authResponse = googleUser.getAuthResponse(true)
+          const accessToken = authResponse.access_token
+          if (!accessToken) {
+            this.onError('Failed get Authorization Data.')
+          }
+          this.contentProgress = 60
+
+          // 画像をGoogleへアップロード(Netlify Function)
+          // Doc: https://developers.google.com/photos/library/guides/upload-media
+          const res = await this.$axios
+            .post('/postPhoto', {
+              params: {
+                accessToken,
+                photoURL: this.imageData
               }
-            )
-            return ret
-          })
-          if (result.status !== 200)
-            this.onError('Failed mediaItems:batchCreate.')
-          this.contentProgress = 100
+            })
+            .catch((e) => this.onError('Failed POST Photo.', e))
+          const uploadToken = res.data
+          this.contentProgress = 75
+
+          if (uploadToken) {
+            this.contentProgress = 95
+            // アップロードした画像をアルバムに追加
+            const result = await this.$gapiInit(this.gapi).then(async () => {
+              // Doc: https://developers.google.com/photos/library/reference/rest/v1/mediaItems/batchCreate
+              const ret = await this.gapi.client.photoslibrary.mediaItems.batchCreate(
+                {
+                  albumId: album.id,
+                  newMediaItems: [
+                    {
+                      description: 'Created by Nest Board.',
+                      simpleMediaItem: {
+                        fileName: this.inputPhotoTitle,
+                        uploadToken
+                      }
+                    }
+                  ]
+                }
+              )
+              return ret
+            })
+            if (result.status !== 200)
+              this.onError('Failed mediaItems:batchCreate.')
+            this.contentProgress = 100
+          } else {
+            this.onError('Failed POST Photo.')
+          }
         } else {
-          this.onError('Failed POST Photo.')
+          this.onError('Failed Album not found.')
         }
-      } else {
-        this.onError('Failed Album not found.')
+      } catch (e) {
+        // eslint-disable-next-line no-console
+        console.trace(e)
       }
       this.processState = this.hasError
         ? PROCCESS_STATE.failed
@@ -298,6 +324,7 @@ export default {
       this.hasError = true
       // eslint-disable-next-line no-console
       console.error(message)
+      throw new Error(message)
     },
     handleModalClose() {
       this.processState = ''
--- app/plugins/gapi-inject.js
+++ app/plugins/gapi-inject.js
@@ -1,19 +1,15 @@
 import firebase, { authConfig } from '~/plugins/firebase'
 
-export default async ({ $axios, store }, inject) => {
-  const gapiScript = document.createElement('script')
-  const src = await $axios.$get('https://apis.google.com/js/api.js')
-  gapiScript.appendChild(document.createTextNode(src))
-  document.head.appendChild(gapiScript)
-  const gapi = window.gapi
-
-  const clientLoad = new Promise((resolve, reject) => {
-    gapi.load('client:auth2', () => {
-      resolve()
+export default ({ store }, inject) => {
+  const clientLoad = (gapi) => {
+    return new Promise((resolve, reject) => {
+      gapi.load('client:auth2', () => {
+        resolve()
+      })
     })
-  })
-  const init = () => {
-    return clientLoad.then(() => {
+  }
+  const init = (gapi) => {
+    return clientLoad(gapi).then(() => {
       return gapi.client.init({
         apiKey: authConfig.Google.apiKey,
         clientId: authConfig.Google.clientId,
@@ -22,12 +18,12 @@ export default async ({ $axios, store }, inject) => {
       })
     })
   }
-  const auth = () => {
-    const auth = init().then(() => gapi.auth2.getAuthInstance())
+  const auth = (gapi) => {
+    const auth = init(gapi).then(() => gapi.auth2.getAuthInstance())
     return auth
   }
-  const signIn = () => {
-    const signIn = auth().then((auth) =>
+  const signIn = (gapi) => {
+    const signIn = auth(gapi).then((auth) =>
       auth.signIn({
         ux_mode: 'redirect',
         scope: authConfig.Google.scopes.join(' ')
@@ -35,8 +31,8 @@ export default async ({ $axios, store }, inject) => {
     )
     return signIn
   }
-  const signOut = () => {
-    const signIn = auth().then((auth) =>
+  const signOut = (gapi) => {
+    const signIn = auth(gapi).then((auth) =>
       auth.signOut().then(() => {
         firebase.auth().signOut()
         store.commit('auth/logout')
@@ -45,29 +41,31 @@ export default async ({ $axios, store }, inject) => {
     )
     return signIn
   }
-
-  // ログイン済みの場合、Firebaseにもログインする
-  const googleAuth = await auth()
-  if (googleAuth.isSignedIn.get()) {
-    const googleUser = googleAuth.currentUser.get()
-    const authResponse = googleUser.getAuthResponse(true)
-    const credential = firebase.auth.GoogleAuthProvider.credential(
-      authResponse.id_token,
-      authResponse.access_token
-    )
-    firebase
-      .auth()
-      .signInWithCredential(credential)
-      .then(({ user }) => {
-        store.dispatch('auth/gotUser', user)
-        store.commit('isReady', true)
-      })
-  } else {
-    store.commit('isReady', true)
+  const currentUser = async (gapi) => {
+    // ログイン済みの場合、Firebaseにもログインする
+    const googleAuth = await auth(gapi)
+    if (googleAuth.isSignedIn.get()) {
+      const googleUser = googleAuth.currentUser.get()
+      const authResponse = googleUser.getAuthResponse(true)
+      const credential = firebase.auth.GoogleAuthProvider.credential(
+        authResponse.id_token,
+        authResponse.access_token
+      )
+      firebase
+        .auth()
+        .signInWithCredential(credential)
+        .then(({ user }) => {
+          store.dispatch('auth/gotUser', user)
+          store.commit('isReady', true)
+        })
+    } else {
+      store.commit('isReady', true)
+    }
   }
-  inject('gapi', gapi)
+
   inject('gapiInit', init)
   inject('googleAuth', auth)
   inject('googleSignIn', signIn)
   inject('googleSignOut', signOut)
+  inject('googleCurrentUser', currentUser)
 }
